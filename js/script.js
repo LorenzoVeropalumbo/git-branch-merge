2c03ac2251724e0a51a1434b4e84f9624460e856
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-08-22 15:58:21 +0200

    fix axios error

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 var app = new Vue (
-    {
-      el: '#root',
-      data: {
-        teams: [],
-        leagues: [],
-        leagueNoDupli: [],
-        value: 'All'
-      },
-      methods: {
-        GetTeams() {
-          axios.get(`http://localhost:8888/git-branch-merge/api.php?league=${this.value}`)
-          .then((response) => {
-            this.teams = response.data;
-          }) 
-        },       
-      },
-      mounted(){
-        this.GetTeams();
-      }
+  {
+    el: '#root',
+    data: {
+      teams: [],
+      leagues: [],
+      leagueNoDupli: [],
+      value: 'All'
+    },
+    methods: {
+      getTeams() {
+        axios.get('http://localhost:8888/git-branch-merge/api.php')
+        .then((response) => {
+          this.teams = response.data;
+          console.log(this.teams);
+        })
+      },       
+    },
+    mounted(){
+      this.getTeams();
     }
-)
+  }
+);
