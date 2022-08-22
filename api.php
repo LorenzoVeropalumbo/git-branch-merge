3074430c49ca2b76cb878961faf9c5a2a683ada4
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-08-22 14:30:28 +0200

    search

--- api.php
+++ api.php
@@ -1,9 +1,27 @@
 <?php
   require __DIR__ . '/database.php';
 
-  $data = [];
+    //creo un array vuoto      
+    $data = [];
     
-  $data = $database;
+    //vedo se nella chiamata axios league a qualche valore 
+    if(!empty($_GET) && $_GET['league'] !== 'All'){
+        
+        foreach ($database as $data) {
+            
+            // controllo che siamo uguali le parole quella cercata dall'utente e quella del database
+            if ($data['league'] === $_GET['league']) {
+                
+                $filtredArray[] = $data;
+            }
+        }
+
+        $data = $filtredArray;
+
+    } else {
+        
+        $data = $database;
+    }
 
   header('Content-Type: application/json');
   
