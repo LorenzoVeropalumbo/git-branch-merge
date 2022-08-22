ece82059cfaa0375288e8545c00c9367711cbe4d
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-22 13:28:34 +0200

    changing api

--- api.php
+++ api.php
@@ -2,17 +2,8 @@
   require __DIR__ . '/database.php';
 
   $data = [];
-
-  if(!empty($_GET) && $_GET['value'] !== 'All'){
-      foreach ($database as $data) {
-          if ($data['genre'] === $_GET['value']) {
-              $filtredArray[] = $data;
-          }
-      }        
-      $data = $filtredArray;
-  } else {
-      $data = $database;
-  }
+    
+  $data = $database;
 
   header('Content-Type: application/json');
   
