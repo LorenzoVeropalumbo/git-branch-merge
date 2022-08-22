5527f8775fcaf9a40b1bdb4270033a9e57d1f93d
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-08-22 15:32:43 +0200

    search

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 var app = new Vue (
     {
-        el: '#root',
-        data: {
-            teams: [],
-            value: 'All'
+      el: '#root',
+      data: {
+        teams: [],
+        leagues: [],
+        leagueNoDupli: [],
+        value: 'All'
+      },
+      methods: {
+        GetTeams() {
+          axios.get(`http://localhost:8888/git-branch-merge/api.php?league=${this.value}`)
+          .then((response) => {
+            this.teams = response.data;
+          }) 
         },
-        methods: {
-            GetTeams() {
-                axios.get(`http://localhost:8888/git-branch-merge/api.php?league=${this.value}`)
-                .then((response) => {
-                    this.teams = response.data;
-                }) 
-            }
-        },
-        mounted(){
-            this.GetTeams();
-        }
+        getLeague() {
+          axios.get(`http://localhost:8888/git-branch-merge/api.php`)
+          .then((response) => {
+            console.log(response.data);
+          }) 
+        }        
+      },
+      mounted(){
+        this.GetTeams();
+        this.getLeague();
+      }
     }
 )
